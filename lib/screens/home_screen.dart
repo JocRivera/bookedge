bfe700a5c1877e1b53fdaf06d0aa5da992a4893b
JocRivera <jose_mrivera@soy.sena.edu.co>
2024-10-25 08:40:31 -0500

    initFuncionando

---
+++ lib/screens/home_screen.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+import '../widgets/sidebar.dart';
+import '../widgets/main_content.dart';
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  final GlobalKey<ScaffoldState> _scaffoldKey = GlobalKey<ScaffoldState>();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      key: _scaffoldKey,
+      appBar: AppBar(
+        title: const Text('Hotel Manager'),
+        leading: IconButton(
+          icon: const Icon(Icons.menu),
+          onPressed: () => _scaffoldKey.currentState?.openDrawer(),
+        ),
+        actions: [
+          PopupMenuButton(
+            icon: const CircleAvatar(
+              backgroundImage: NetworkImage('https://via.placeholder.com/32'),
+            ),
+            itemBuilder: (BuildContext context) => [
+              const PopupMenuItem(
+                child: Text('Profile'),
+              ),
+              const PopupMenuItem(
+                child: Text('Settings'),
+              ),
+              const PopupMenuItem(
+                child: Text('Log out'),
+              ),
+            ],
+          ),
+        ],
+      ),
+      drawer: const Sidebar(),
+      body: const MainContent(),
+    );
+  }
+}
---
+++ lib/widgets/main_content.dart
@@ -0,0 +1,67 @@
+import 'package:flutter/material.dart';
+
+class MainContent extends StatelessWidget {
+  const MainContent({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SingleChildScrollView(
+      padding: const EdgeInsets.all(16),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          const Text(
+            'Welcome to the Lobby',
+            style: TextStyle(
+              fontSize: 24,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+          const SizedBox(height: 16),
+          GridView.count(
+            shrinkWrap: true,
+            crossAxisCount: 2,
+            crossAxisSpacing: 16,
+            mainAxisSpacing: 16,
+            children: [
+              _buildMenuButton(
+                context,
+                icon: Icons.bed,
+                label: 'Cabins & Rooms',
+                onPressed: () {},
+              ),
+              _buildMenuButton(
+                context,
+                icon: Icons.calendar_today,
+                label: 'Reservations',
+                onPressed: () {},
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget _buildMenuButton(
+    BuildContext context, {
+    required IconData icon,
+    required String label,
+    required VoidCallback onPressed,
+  }) {
+    return ElevatedButton(
+      onPressed: onPressed,
+      style: ElevatedButton.styleFrom(
+        padding: const EdgeInsets.all(16),
+      ),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Icon(icon, size: 32),
+          const SizedBox(height: 8),
+          Text(label),
+        ],
+      ),
+    );
+  }
+}
